d14ab09b201c8c975fbb0db4944202c26f3e0493
Vincent <1075021604@qq.com>
2019-08-15 18:10:34 +0800

    library_member 会员模块 练习代理继承

--- odoo-12.0/my-modules/library_app/models/library_book.py
+++ odoo-12.0/my-modules/library_app/models/library_book.py
@@ -14,6 +14,7 @@ class Book(models.Model):
     active = fields.Boolean('Active?', default=True)
     date_published = fields.Date()
     image = fields.Binary('Cover')
+
     publisher_id = fields.Many2one('res.partner', string='Publisher')
     author_ids = fields.Many2many('res.partner', string='Authors')
 
